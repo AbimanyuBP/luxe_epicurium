69ac974a2185ea8e46e3e419538a769f2bd7a220
Abimanyu B.P <abimanyubevanp@gmail.com>
2023-09-14 08:58:02 +0700

    Added missing database schema and remigrate

---
+++ database/migrations/2023_09_13_144107_create_reviews_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id('review_id');
+            $table->foreignId('user_id');
+            $table->foreignId('restaurant_id');
+            $table->float('rating');
+            $table->text('comment');
+            $table->dateTime('date_created');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
