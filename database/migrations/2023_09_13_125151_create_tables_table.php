c4fedc17ab73eaec53186c69d68c1d97fa1ed1c6
Abimanyu B.P <abimanyubevanp@gmail.com>
2023-09-14 11:29:08 +0700

    Added missing fields and remigrate

--- database/migrations/2023_09_13_125151_create_tables_table.php
+++ database/migrations/2023_09_13_125151_create_tables_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('tables', function (Blueprint $table) {
             $table->id('table_id');
             $table->foreignId('restaurant_id');
+            $table->integer('capacity');
             $table->string('table_number');
             $table->text('description'); // describe the table in short detail
             $table->string('accessibility'); // define how the table can accomodate specific client needs, wheel chair accessible, for big groups, sunlit, near a window, etc
